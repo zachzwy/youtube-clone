7314a40e73334293e45ce84c5c43dd07405524f6
wenyu <wenyu@wustl.edu>
2019-08-11 22:21:18 -0700

    Updates

---
+++ precache-manifest.f0fb0a72a47689cc39b2572015882c19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7f777a9da8e35544b57fdcd302e7a9",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "8c51c1bcc88b5ca058cb",
+    "url": "/youtube-clone/static/css/main.3a0cbb79.chunk.css"
+  },
+  {
+    "revision": "de68fd8fae509464ab29",
+    "url": "/youtube-clone/static/js/2.2a8182fe.chunk.js"
+  },
+  {
+    "revision": "8c51c1bcc88b5ca058cb",
+    "url": "/youtube-clone/static/js/main.fb3c289b.chunk.js"
+  },
+  {
+    "revision": "a15bf1e9bf9a5f14bed6",
+    "url": "/youtube-clone/static/js/runtime~main.ee0b5924.js"
+  }
+]);
