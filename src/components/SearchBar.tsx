a68c0b0848a0045d83872e69eb7a8f271cef4432
wenyu <zachzwy@gmail.com>
2019-10-27 20:52:30 -0700

    Update committer

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createRef } from "react";
+import React, { createRef } from "react";
 import { Paper, TextField } from "@material-ui/core";
 
 const SearchBar = ({ onFormSubmit }: { onFormSubmit: Function }) => {
